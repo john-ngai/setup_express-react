2e23ad34cc1d4e8668e61941057b91b330fd59bf
mrjohnming <john.ngai@hotmail.com>
2022-04-07 13:36:04 -0400

    Add request - fetch /api

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => setData(data.message));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header">  
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
 }
-
-export default App;
