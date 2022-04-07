966acf536611716c2a9476608df96a1cdbc74f32
mrjohnming <john.ngai@hotmail.com>
2022-04-07 13:38:47 -0400

    Update route - GET /api

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server!' });
+  res.send({ message: 'Hello from server!' });
 });
 
 app.listen(PORT, () => {
