a301fcd02a256c1945a17418584729b123222229
mrjohnming <john.ngai@hotmail.com>
2022-04-11 21:24:32 -0400

    Add route - react app

--- server/index.js
+++ server/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Have Node serve the files for our built React app.
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+// GET /api.
 app.get('/api', (req, res) => {
   res.send({ message: 'Hello from server!' });
+});
+
+// All other GET requests not handled before will return our React app.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
